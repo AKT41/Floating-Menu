42c7c121c0cd728a1923ae18f5d96021d078fbf2
DESKTOP-CT44TRH\ahmet <ahmetturker0541@gmail.com>
2022-12-07 09:30:40 +0300

    floating menu with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
 
-function App() {
+export default function App() {
+  const [position, setPosition] = useState("");
+  const clickhandle = (e) => {
+    const { top, width, height } = e.target.getBoundingClientRect();
+    const left = e.target.offsetLeft
+    setPosition({
+      left,
+      top,
+      width,
+      height
+    });
+  };
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="menu">
+        {Object.values(position).length > 0 && (
+          <div
+            className="divider"
+            style={{
+              "--left": position.left + "px",
+              "--top": position.top + "px",
+              "--width": position.width + "px",
+              "--height": position.height + "px"
+            }}
+          />
+        )}
+        {Object.values(position).length > 0 && (
+          <div
+            className="divider1"
+            style={{
+              "--left": position.left + "px",
+              "--top": position.top + "px",
+              "--width": position.width + "px",
+              "--height": position.height + "px"
+            }}
+          />
+        )}
+        <button onClick={clickhandle} >Ana sayfa </button>
+        <button onClick={clickhandle} >İletişim</button>
+        <button onClick={clickhandle} >Dersler</button>
+        <button onClick={clickhandle} >Makaleler</button>
+      </nav>
+    </>
   );
 }
-
-export default App;
